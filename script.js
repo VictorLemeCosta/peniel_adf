3834926f31f663dc214df5a0733fdf20524267ff
VICTOR LEME COSTA <88389939+VictorLemeCosta@users.noreply.github.com>
2023-07-25 17:25:52 -0300

    commit 1 25/07/2023
    melhoria menu responsivo

---
+++ script.js
@@ -0,0 +1,96 @@
+'use strict'
+
+let open = false
+
+document.querySelector('#btnMenu').addEventListener('click', e => {
+    open = !open;
+    toggleMenu();
+})
+
+document.querySelector('#botao1').addEventListener('click', e => {
+    open = false;
+    toggleMenu();
+    trocarImagem();
+})
+
+document.querySelector('#botao2').addEventListener('click', e => {
+    open = false;
+    toggleMenu();
+    trocarImagem();
+})
+
+document.querySelector('#botao3').addEventListener('click', e => {
+    open = false;
+    toggleMenu();
+    trocarImagem();
+})
+
+document.querySelector('#botao4').addEventListener('click', e => {
+    open = false;
+    toggleMenu();
+    trocarImagem();
+})
+
+function toggleMenu() {
+    if (open) {
+        /*Abre o menu*/
+        document.querySelector('#menu').style.visibility = 'visible';
+        document.querySelector('#menu').style.opacity = '1';
+        document.querySelector('#menu').style.transition = "opacity .7s ease-in-out";
+        return;
+    }
+    /*Fecha o menu*/
+    document.querySelector('#menu').style.opacity = '0';
+    document.querySelector('#menu').style.transition = "opacity .7s ease-in-out";
+    setTimeout(function(){
+         document.querySelector('#menu').style.visibility = 'hidden';
+    }, 700);
+}
+
+/*function changeImage(a) {
+    document.getElementById("#btnClose").src=a;
+}*/
+
+window.onscroll = function() {
+    scrollFunction()
+};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 ||
+        document.documentElement.scrollTop > 40)
+    {
+        document.getElementById("topo").style.height = "60px";
+              
+        document.getElementById("logo").style.width = "45px";
+
+        document.getElementById("topo").style.transition = ".5s";
+
+        document.getElementById("logo").style.transition = ".5s";
+
+        document.getElementById("menu").style.top = "60px";
+
+        document.getElementById("menu").style.transition = ".5s";
+    } 
+    else {
+        document.getElementById("topo").style.height = "100px";
+                      
+        document.getElementById("logo").style.width = "80px";
+
+        document.getElementById("menu").style.top = "100px";
+
+        document.getElementById("menu").style.transition = ".5s"
+    }
+}
+
+const btnImage = document.getElementById("btnMenu");
+
+function trocarImagem() {
+    if (open == false) {
+        btnImage.src = "./assets/menu-fechado.png";
+        return;
+    } else {
+        btnImage.src = "./assets/menu-aberto.png";
+        return;
+    }
+  }
+  document.getElementById("btnMenu").addEventListener("click", trocarImagem);
